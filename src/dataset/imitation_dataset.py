fixed bug in sensor dataloader, added loss calculation before epoch 0, put parts of model in eval mode when freezing weights

src/dataset/imitation_dataset.py
@@ -32,7 +32,10 @@ class SequentialTorchDataset(Dataset):
 
     Args:
         Dataset ([hparams]): the hyperparamters
-        dataset_type ([string]): train, test, or val 
+        dataset_type ([string]): train, test, or val
+    
+    Notes:
+    - data stats are: (mean=[0.384, 0.389, 0.403], std=[0.126, 0.123, 0.126]) 
 
     Returns:
         [tuple of (list) of torch tensors]: input (may contain output) consisting of images,
@@ -43,23 +46,35 @@ class SequentialTorchDataset(Dataset):
         self.hparams = hparams
         self.image_files = dict.fromkeys(self.hparams['modalities'])
         self.semantic = 'semantic_label' in self.image_files.keys()
+        self.dataset_type = dataset_type
 
 
         self.read_path = hparams['data_dir']+ dataset_type + '/'
         print(self.read_path)
 
-        for modal in self.image_files.keys():
-            self.image_files[modal] = sorted(glob(self.read_path +'*/'+ modal + '/*'))
+        for modal in self.image_files.keys(): # sort by image names
+            paths = glob(self.read_path +'*/'+modal+'/*')
+            self.image_files[modal] = sorted(paths, key=lambda x: int(x.split(modal+'/')[1].split('.')[0]))
 
         self.file_idx = [int(name.split('/')[-1].split('.')[0]) for name in self.image_files['camera']]
         self.file_idx = (np.array(self.file_idx) - self.file_idx[0]).tolist()  # always start at 0
 
-        assert len(self.file_idx) == self.file_idx[-1]+1 == len(self.image_files['camera'])
+        assert len(self.file_idx) == self.file_idx[-1]+1 == len(self.image_files['camera']), '{} {} {}'.format(
+           len(self.file_idx), self.file_idx[-1]+1, len(self.image_files['camera']))
 
         # To combbine the data fromes if havent already done so
         if not os.path.exists(self.read_path + 'sensor_all.json'):
             self._combineSensorDataframes()
         self._load_sensor()
+
+        # Transforms
+        self.transforms = transforms.Compose([
+            # transforms.Normalize(mean=[0.384, 0.389, 0.403], std=[0.126, 0.123, 0.126]),
+            # transforms.Grayscale(),
+            transforms.ColorJitter(brightness=.5, contrast=.5),
+            transforms.GaussianBlur(kernel_size=(3, 3), sigma=(0.05, 1.5)),
+            transforms.RandomAdjustSharpness(2, p=0.5)
+        ]) 
 
     def _load_sensor(self):
         '''Load the sensor data and labels from a single json file
@@ -76,8 +91,8 @@ class SequentialTorchDataset(Dataset):
 
         sensor = np.vstack([np.array(sensor_df['command'].tolist()),                               # sensor data
                         np.linalg.norm(np.array(sensor_df['velocity'].tolist()), axis=1),
-                        np.linalg.norm(np.array(sensor_df['acceleration'].tolist()), axis=1)
-                    ]).T
+                        # np.linalg.norm(np.array(sensor_df['acceleration'].tolist()), axis=1),
+                    ]).T 
 
         target = np.stack((redlight_status, action_ind, redlight_dist, frontcar_dist), axis=-1)
         
@@ -105,7 +120,7 @@ class SequentialTorchDataset(Dataset):
         '''
         # categories: moving obstacles, traffic lights, road markers, road, sidewalk and background
         segclass_colors = {
-            0: [70, 70, 70],         # 0: Building 1, Fence 2, Other 3, poles 5, vegetation 9, walls 11
+            0: [70, 70, 70],       # 0: Building 1, Fence 2, Other 3, poles 5, vegetation 9, walls 11
             1: [0, 0, 142],        # 1: pedestrian 4, vehicles 10
             2: [220, 220, 0],      # 2: traffic signs 12, 
             3: [153, 153, 153],    # 3: roadlines 6,
@@ -122,7 +137,8 @@ class SequentialTorchDataset(Dataset):
     def _combineSensorDataframes(self):
         '''Combines sensor data from different logs into a single json file
         '''
-        files = sorted(glob( self.read_path + '*/*.json'))
+        paths = glob(self.read_path + '*/*.json')
+        files = sorted(paths, key=lambda x: int(x.split('sensor_')[1].split('.')[0]))
         dfs = []
         for i in tqdm.tqdm(range(len(files)), desc='Combining sensor data'):
             dfs.append(pd.read_json(files[i], orient='index'))
@@ -145,11 +161,11 @@ class SequentialTorchDataset(Dataset):
 
         assert len(image_array.shape) == 3
         image_array = image_array[0,:,:]                                            # classes encocded in red channel
-        resized_image_array = cv2.resize(image_array, (128,74), cv2.INTER_NEAREST)  # Note: axis are flipped in cv
+        image_array = cv2.resize(image_array, (128,74), cv2.INTER_NEAREST)          # Note: axis are flipped in cv
 
-        semseg_target = np.zeros_like(resized_image_array)
+        semseg_target = np.zeros_like(image_array)
         for _, (clss, val) in enumerate(segclass.items()):
-            mask = np.isin(resized_image_array, val)
+            mask = np.isin(image_array, val)
             semseg_target[np.where(mask)] = clss
         
         return semseg_target  # num_images * height * width
@@ -170,7 +186,7 @@ class SequentialTorchDataset(Dataset):
 
         images = np.moveaxis(images, -1,-3)                 # need [ch,height,width] or [num_imgs,ch,height,width]
         if self.hparams['crop_sky']:
-            images = images[...,120:,:]                     # if crop_sky
+            images = images[...,100:,:]                     # if crop_sky
 
 
         if modality =='camera':
@@ -203,6 +219,8 @@ class SequentialTorchDataset(Dataset):
         # Load the image
         x = self._load_file(index, mode=self.hparams['loading_mode'])
         x = torch.from_numpy(x).type(torch.float32)
+        if self.hparams['data_augmentation'] and self.dataset_type=='train':
+            x = self.transforms(x)
 
         x_sem = 0
         if self.semantic: 
@@ -224,7 +242,7 @@ def sequential_train_val_test_iterator(hparams, modes=['train', 'val', 'test']):
     '''
     data_iterator = {}
     for mode in modes:
-        data = SequentialTorchDataset(hparams, dataset_type=mode)
+        data = SequentialTorchDataset(hparams, dataset_type=mode+'_trash')
         data_iterator[mode+'_dataloader'] = DataLoader(data,
                                                        batch_size=hparams['BATCH_SIZE'],
                                                        shuffle=False, num_workers=hparams['NUM_WORKERS']
@@ -288,3 +306,19 @@ def clb_weight(y):
 
     # class_weight = np.reciprocal(traindata_sum, where = traindata_sum > 0)
     class_weight = torch.from_numpy(class_weight)
+
+def calculateStats(dataloader):
+    '''calcualte the statistics of the image data
+    '''
+    means, stds = [], []
+    for i, batch in enumerate(dataloader):
+        x, y = batch
+        x[0] = x[0].to(torch.device('cuda:0'))
+        # x[1] = x[1].to(torch.device('cuda:0'))
+
+        # To calculate mean and std
+        means.append(torch.mean(x[0], dim =(0,2,3)).cpu().numpy())
+        stds.append(torch.std(x[0], dim =(0,2,3)).cpu().numpy())
+
+    print('means',np.mean(means, axis=0))
+    print('stds',np.mean(stds, axis=0))

src/models/imitation.py
@@ -15,37 +15,37 @@ from matplotlib import pyplot as plt
 
 
 
-def lossCriterion(obj, inp, out):
-    l_act, l_sem, l_tr, l_tr_dist, l_car_dist = 0, 0, 0, 0, 0 
-
+def lossCriterion(obj, inp, out, validation=False):
+    '''The auxiliary loss function
+    '''
+    losses={} 
     # model output: out_seg, tl_state_output, dist_to_tl_output, dist_to_frontcar
-    ### calculate loss
-    # regression
-    # l1 = nn.functional.mse_loss(inp[0], out[0][0])                              # image reconstruction MSE
-    # l1 = 1 - ms_ssim(inp[0], out[0][0], data_range=1, size_average=True)        # image reconstruction
-    # l1 = 1 - ms_ssim(inp[0], out[0][2], data_range=1, size_average=True)        # semantic segmentation
 
+    ### classification
+    # losses['image_r'] = 1 - ms_ssim(inp[0], out[0][0], data_range=1, size_average=True)   # image reconstruction
+    # losses['semantic'] = nn.functional.cross_entropy(inp[0], out[0][2])                   # pixel-wise semantic segmentation
 
-    # l1 = 1 - ms_ssim(inp[0], out[0][0], data_range=1, size_average=True)        # image reconstruction
-    
-    # classification
-    # l_act = nn.functional.cross_entropy(inp[4], out[1][:,1])                      # Autopilot Action
-    l_sem = nn.functional.cross_entropy(inp[0], out[0][2])                        # pixel-wise semantic segmentation
-    # l_tr = nn.functional.cross_entropy(inp[1], out[1][:,0])                       # trafficlight status detection
-    # l_tr_dist = nn.functional.cross_entropy(inp[2], out[1][:,2])                  # dist to traffic light
-    # l_car_dist = nn.functional.cross_entropy(inp[3], out[1][:,3])                 # dist to front car
-   
-    ### plot loss
-    # print('loss:', l_sem.item(), l_tr.item(), l_tr_dist.item(), l_car_dist.item())
-    obj.log('autopilot_action_loss', l_act.item(), on_step=False, on_epoch=True)
-    # obj.log('image_loss', l_sem.item(), on_step=False, on_epoch=True)
-    # obj.log('traffic_loss', l_tr.item(), on_step=False, on_epoch=True)
-    # obj.log('traffic_dist_loss', l_tr_dist.item(), on_step=False, on_epoch=True)
-    # obj.log('frontcar_dist_loss', l_car_dist.item(), on_step=False, on_epoch=True)
+    ### Class balanced
+    losses['act'] = nn.functional.cross_entropy(inp[4], out[1][:,1], obj.cb_weight['act'])   # Autopilot Action
+    # losses['tr_status'] = nn.functional.cross_entropy(inp[1], out[1][:,0], obj.cb_weight['tr_status'])  # trafficlight status detection
+    # losses['tr_dist'] = nn.functional.cross_entropy(inp[2], out[1][:,2], obj.cb_weight['tr_dist'])      # dist to traffic light
+    # losses['car_dist'] = nn.functional.cross_entropy(inp[3], out[1][:,3], obj.cb_weight['car_dist'])    # dist to front car
 
+    ### NOT CB
+    # losses['tr_status'] = nn.functional.cross_entropy(inp[1], out[1][:,0])  # trafficlight status detection
+    # losses['tr_dist'] = nn.functional.cross_entropy(inp[2], out[1][:,2])    # dist to traffic light
+    # losses['car_dist'] = nn.functional.cross_entropy(inp[3], out[1][:,3])   # dist to front car
 
-    ### weighted summation
-    loss = l_sem + l_tr + l_tr_dist + l_car_dist + l_act
+    loss = 0
+    for _, (k, val) in enumerate(losses.items()):
+        # print('{}: {:.2f}'.format(k, val.item()), end='\t')
+        if not validation:
+            obj.log(k+'_loss', val.item(), on_step=False, on_epoch=True)
+        else:
+            obj.log(k+'_loss_val', val.item(), on_step=False, on_epoch=True)
+        loss += val * obj.loss_factor[k]
+    # print()
+
     return loss
 
 
@@ -59,18 +59,42 @@ class Imitation(pl.LightningModule):
         self.criterion = lossCriterion
         self.before_train()
 
+        self.val_loss_in_valStep = True
+
     def forward(self, x):
         output = self.net.forward(x)
         return output
     
     def before_train(self):
-        cur_time = str(datetime.now())
-        info = cur_time +': just all aux encoder + single img in single out + PRETRAINED RESNET+ NO action\n'
+
+        ### logging
+        info = ('\n' + str(datetime.now()) +': Just Image Reconstruction + No Data Augmentation  + reg=0 + NO action' #+
+         #' + 256 latent CB_LOSS for all except semseg + Factor 3 for semseg\n'
+        )
         print(info)
         with open(self.h_params.log_dir + 'info.txt', 'a') as f:
             f.write(info)
-        class_weight = 1.0/np.array([45150, 21977, 17134, 181751])
-        self.cb_weight = torch.from_numpy(class_weight).type(torch.float32).to(torch.device('cuda:0'))
+
+        ### class-balanced loss
+        distributions = {
+            'tr_status':{0: 118975, 1: 35064, 2: 356261},
+            'tr_dist': {0: 40710, 1: 54691, 2: 28645, 3: 386254},
+            'car_dist':{0: 91403, 1: 17656, 2: 11073, 3: 390168},
+            'act': {0: 5859, 1: 27987, 2: 230868, 3: 23617, 4: 7574, 5:0, 6: 1569, 7: 683, 8: 316, 9:0, 10: 13136,
+             11: 20619, 12: 101168, 13: 16284, 14: 5649, 15: 2284, 16: 18450, 17: 13646, 18: 19437, 19: 1154}
+        }
+        self.cb_weight = {}
+        for _, (key, val) in enumerate(distributions.items()):
+            occurences = np.array(list(val.values()))
+            occurences[np.where(occurences==0)] = np.max(occurences)
+            class_weight = 1.0/occurences
+            self.cb_weight[key] = torch.from_numpy(class_weight).type(torch.float32).to(torch.device('cuda:0'))
+        self.loss_factor={'semantic':3, 'act':1, 'image_r':1, 'tr_status':1 ,'tr_dist':1 , 'car_dist': 1}
+
+    def on_train_start(self) -> None:
+        self.val_loss_in_valStep = True
+        # self.trainer.run_evaluation()
+        # self.trainer.checkpoint_callback.on_train_start(self.trainer, pl_module)
 
     def training_step(self, batch, batch_idx):
         x, y = batch
@@ -89,9 +113,13 @@ class Imitation(pl.LightningModule):
         # Predict and calculate loss
         output = self.forward(x)
         target = [x,y]
-        loss = self.criterion(self, output, target)
 
-        self.log('val_loss', loss, on_step=False, on_epoch=True)
+        if self.val_loss_in_valStep:
+            loss = self.criterion(self, output, target, validation=True)
+            self.log('val_loss', loss, on_step=False, on_epoch=True)
+        else:  # To Log loss before training
+            loss = self.criterion(self, output, target, validation=False)
+            self.log('train_loss', loss, on_step=False, on_epoch=True)
         return loss
 
     def train_dataloader(self):
@@ -109,30 +137,29 @@ class Imitation(pl.LightningModule):
                     weight_decay = self.h_params['WEIGHT_DECAY']
                     )
 
-    def scale_image(self, img):
-        out = (img + 1) / 2
-        return out
-
 
     def calcAccuracy(self, dataset_type='val'):
+        ''' calculate the per-class accuaracy and plot confusion matrix
+        NOTE: must have <255 classes or else remove astype(np.uint8) used to reduce size
+        '''
+       
+        ### select keys in the auxiliary model
+        keys = ['dist_car', 'traffic', 'dist_tr']
+        # keys = ['act']
+        # keys = ['semseg']
+        
         self.net.eval()
         with torch.no_grad():
             net = self.net.to(torch.device('cuda:0'))
             dataloader = self.data_loader[dataset_type+'_dataloader']
-            ### keys in the auxiliary model
-            # keys = ['act', 'dist_car', 'traffic', 'dist_tr']
-            # keys = ['act']
-            keys = ['semseg']
             # intialization
             pred_bt = dict.fromkeys(keys)
             labels_bt = dict.fromkeys(keys)
-            pred_total = dict.fromkeys(keys, torch.zeros(0,dtype=torch.long, device='cpu'))
-            labels_total = dict.fromkeys(keys, torch.zeros(0,dtype=torch.long, device='cpu'))
+            pred_total = {key:[] for key in keys}
+            labels_total = {key:[] for key in keys}
 
             for i, batch in enumerate(dataloader):
                 x, y = batch
-                # for it in x:
-                #     it = it.to(torch.device('cuda:0'))
                 x[0] = x[0].to(torch.device('cuda:0'))
                 x[1] = x[1].to(torch.device('cuda:0'))
 
@@ -158,23 +185,24 @@ class Imitation(pl.LightningModule):
                     labels_bt['semseg'] = target[0][2].cpu()
                 
                 
-
-
                 print('\nbatch {}/{} acc - '.format(i,len(dataloader)), end='')
                 for key in keys:
-                    pred_total[key] = torch.cat((pred_total[key], pred_bt[key].view(-1)))
-                    labels_total[key] = torch.cat((labels_total[key], labels_bt[key].view(-1)))
+                    pred_total[key].append(pred_bt[key].view(-1).numpy().astype(np.uint8))      # cast to uint8 to reduce size
+                    labels_total[key].append(labels_bt[key].view(-1).numpy().astype(np.uint8))
                     batchacc = 100*accuracy_score(labels_bt[key].view(-1), pred_bt[key].view(-1))
                     print('{}: {:.1f}'.format(key, batchacc), end='\t')
 
 
             for key in keys:
                 print('\n*******************\nAux Task:',key)
-                conf_mat = confusion_matrix(labels_total[key].numpy(), pred_total[key].numpy())
+                labels_total_flat = np.concatenate(labels_total[key]).ravel()
+                pred_total_flat = np.concatenate(pred_total[key]).ravel()
+
+                conf_mat = confusion_matrix(labels_total_flat, pred_total_flat)
                 print(conf_mat)
                 # Per-class accuracy
                 perclass_acc = np.around(100*conf_mat.diagonal()/conf_mat.sum(1), decimals=1)
-                acc = 100*accuracy_score(labels_total[key].numpy(), pred_total[key].numpy())
+                acc = 100*accuracy_score(labels_total_flat, pred_total_flat)
                 print('Per-class accuracy:', perclass_acc)
                 print('Accuracy:{:.2f}'.format(acc))
                 
@@ -186,30 +214,34 @@ class Imitation(pl.LightningModule):
 
 
     def sampleOutput(self, dataset_type='val'):
+        dataloader = self.data_loader[dataset_type + '_dataloader']
+        plotSemseg = dataloader.dataset.plotSemseg      # for plotting the semseg
         self.net.eval()
         with torch.no_grad():
             net = self.net.to(torch.device('cuda:0'))
-            dataloader = self.data_loader[dataset_type + '_dataloader']
-            plotSemseg = dataloader.dataset.plotSemseg      # for plotting the semseg
 
-            f, axarr = plt.subplots(1,2)
+            array2, array3 = None, None
+            f, axarr = plt.subplots(2,2)
             for i, batch in enumerate(dataloader):
                 x, y = batch
                 x[0] = x[0].to(torch.device('cuda:0'))
                 x[1] = x[1].to(torch.device('cuda:0'))
                 output = net(x)
-                ind = 0
                 b_ind = random.randint(0, self.h_params['BATCH_SIZE']-1)        # index in current batch 
 
                 ### plot image (multiple images mode)
+                # ind = 0
                 # array1 = x[0][b_ind][ind].cpu().detach().numpy()
                 # array2 = output[0][b_ind][ind].cpu().detach().numpy()
 
-                ### plot single image
+                ### plot single original input image
                 array1 = x[0][b_ind][:].cpu().detach().numpy()
-                array1 = np.moveaxis(array1, 0, 2)
+                array1 = 255 * np.moveaxis(array1, 0, 2)
+
+                #################################
+                ### reconstructed image
                 array2 = output[0][b_ind][:].cpu().detach().numpy()
-                array2 = np.moveaxis(array2, 0, 2)
+                array2 = 255 * np.moveaxis(array2, 0, 2)
                 
                 #################################
                 # ### semseg model
@@ -217,14 +249,24 @@ class Imitation(pl.LightningModule):
                 # array2 = plotSemseg(array2)
 
                 ## semseg ground truth
-                # array2 = x[2][b_ind][:].cpu().detach().numpy()
-                # array2 = plotSemseg(array2)
+                # array3 = x[2][b_ind][:].cpu().detach().numpy()
+                # array3 = plotSemseg(array3)
+   
 
                 ###################################
                 ###################################
                 ### Plot the image
-                axarr[0].imshow(np.uint8(array1),)
-                axarr[1].imshow(np.uint8(array2),)
+                axarr[0,0].imshow(np.uint8(array1),)
+                axarr[0,0].title.set_text('original image')
+
+                if array3 is not None:                
+                    axarr[1,0].imshow(np.uint8(array3),)
+                    axarr[1,0].title.set_text('ground-truth')
+                
+                if array2 is not None:  
+                    axarr[1,1].imshow(np.uint8(array2),)
+                    axarr[1,1].title.set_text('semseg output')
+
 
                 # axarr[0].imshow(np.uint8(array1*255), cmap='gray',)
                 # axarr[1].imshow(np.uint8(array2*255), cmap='gray',)

src/models/model_supervised.py
@@ -38,20 +38,14 @@ def create_resnet_basic_block(
 class Model_Segmentation_Traffic_Light_Supervised(nn.Module):
     def __init__(
         self,
-        nb_images_input,
-        nb_images_output,
-        hidden_size,
-        nb_class_segmentation,
-        nb_class_dist_to_tl,
-        crop_sky=False,
-        pretrained = False
+        hparams
     ):
         super().__init__()
-        if crop_sky:
+        if hparams['crop_sky']:
             self.size_state_RL = 6144
         else:
             self.size_state_RL = 8192
-        resnet18 = models.resnet18(pretrained=pretrained)
+        resnet18 = models.resnet18(pretrained=hparams['pretrained'])
         
         # See https://arxiv.org/abs/1606.02147v1 section 4: Information-preserving
         # dimensionality changes
@@ -80,21 +74,28 @@ class Model_Segmentation_Traffic_Light_Supervised(nn.Module):
         assert resnet18.layer4[0].downsample[0].stride == (2, 2)
 
         self.example_input_array = [torch.randn((32, 3, 168, 288)), torch.randn((32, 3))]
-        self.semseg = True
-        self.traffic_status = False
-        self.traffic_dist = False
-        self.dist_car = False
-        self.action = False
+        self.semseg = hparams['semseg']
+        self.traffic_status = hparams['traffic_status']
+        self.traffic_dist = hparams['traffic_dist']
+        self.dist_car = hparams['dist_car']
+        self.action = hparams['action']
 
-        self.use_aux = True
-        self.use_sensor = True
+        self.use_aux =  hparams['use_aux']
+        self.use_sensor = hparams['use_sensor']
+
+        nb_images_input = hparams['nb_images_input']
+        nb_images_output = hparams['nb_images_output']
+        hidden_size = hparams['hidden_size']
+        nb_class_segmentation = hparams['nb_class_segmentation']
+
+
 
         ### to adapt to the number of input
-        if nb_images_input != 1:
-            new_conv1 = nn.Conv2d(
-                nb_images_input * 3, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False
-            )
-            resnet18.conv1 = new_conv1
+        # if nb_images_input != 1:
+        # new_conv1 = nn.Conv2d(
+        #     nb_images_input * 3, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False
+        # )
+        # resnet18.conv1 = new_conv1
 
         self.encoder = nn.Sequential(
             *(list(resnet18.children())[:-2])
@@ -115,11 +116,9 @@ class Model_Segmentation_Traffic_Light_Supervised(nn.Module):
 
         if self.dist_car:
             # classification on the distance to the front car
-            self.fc_dist_to_frontcar = nn.Sequential(
-                nn.Linear(self.size_state_RL, hidden_size),
-                nn.LeakyReLU(),
-                nn.Linear(hidden_size, 4)
-            )
+
+            self.fc1_dist_to_frontcar = nn.Linear(self.size_state_RL, hidden_size)
+            self.fc2_dist_to_frontcar = nn.Linear(hidden_size, 4)
 
 
         if self.semseg: 
@@ -130,7 +129,7 @@ class Model_Segmentation_Traffic_Light_Supervised(nn.Module):
             self.up_sampled_block_2 = create_resnet_basic_block(24, 32, 256, 128)
             self.up_sampled_block_3 = create_resnet_basic_block(48, 64, 128, 64)
             self.up_sampled_block_4 = create_resnet_basic_block(74, 128, 64, 32)   # for semseg
-            # self.up_sampled_block_4 = create_resnet_basic_block(168, 288, 64, 32)    # for image reconstruction  
+            # self.up_sampled_block_4 = create_resnet_basic_block(188, 288, 64, 32)    # for image reconstruction  
 
             self.last_conv_segmentation = nn.Conv2d(
                 32,
@@ -148,7 +147,7 @@ class Model_Segmentation_Traffic_Light_Supervised(nn.Module):
             )
 
         aux_len = int(self.use_aux) * (3 + 4 + 4)
-        sensor_len = int(self.use_sensor) * 3
+        sensor_len = int(self.use_sensor) * 2
 
         if self.action:
             num_actions = 20
@@ -167,28 +166,28 @@ class Model_Segmentation_Traffic_Light_Supervised(nn.Module):
     def freezeLayers(self, selected_subnet=['encoder','decoder'], exclude_mode=False):
         '''Freezes selected subnet layers (or everything else if exclude_mode is true)
         '''
-        print('Frezing layers ...')
-        # for name, param in self.named_children():
-        #     print(name)
-
-        print('ending\n')
+        print('Frezing layers ...\nPutting modules in eval() mode:')
+        for name, param in self.named_children():
+            for subnet in selected_subnet: 
+                if not exclude_mode and subnet in name: 
+                    param.eval()
+                    print('\t',name)
+                elif exclude_mode and subnet not in name:
+                    param.eval()
+                    print('\t',name)
+        print('Finshed putting modules in eval mode\n')
 
         for name, param in self.named_parameters():
             for subnet in selected_subnet: 
                 if not exclude_mode and subnet in name: 
                     param.requires_grad = False
-                    # param.eval()
                 elif exclude_mode and subnet not in name:
                     param.requires_grad = False
-                    # print(name)
-                    # param.eval()
-                    # if 'running' in name:
-                    # if isinstance(param, torch.nn.modules.batchnorm._BatchNorm):
 
         print('\nFozen selected layers. Trainable weights are:')
         for name, param in self.named_parameters():
             if param.requires_grad:
-                print(name)
+                print('\t',name)
 
     def loadWeights(self, ckpt_path, selected_subnet=['encoder', 'decoder'], exclude_mode=False):
         '''loads selected subnet weights (or everything else if exclude_mode is true)
@@ -202,20 +201,20 @@ class Model_Segmentation_Traffic_Light_Supervised(nn.Module):
                 if not exclude_mode and subnet in name:
                     tr_param = trained_params['net.' + name]
                     param.copy_(tr_param)
-                    print(subnet)
+                    print('\t',subnet)
                 elif exclude_mode and subnet not in name: 
                     tr_param = trained_params['net.' + name]
                     param.copy_(tr_param)
-                    print(name)
+                    print('\t',name)
 
         print('Loaded selected weights complete!***************************\n')
 
 
     def forward(self, x):
 
+        # with torch.no_grad():
         # Encoder first, resnet18 without last fc and abg pooling
         encoding = self.encoder(x[0])  # 512*4*4 or 512*4*3 (crop sky)
-        # print('encoding shape', encoding.shape)
 
         encoding = self.last_conv_downsample(encoding)
 
@@ -227,10 +226,14 @@ class Model_Segmentation_Traffic_Light_Supervised(nn.Module):
             upsample2 = self.up_sampled_block_2(upsample1)  # 128*32*32 or 128*24*32 (crop sky)
             upsample3 = self.up_sampled_block_3(upsample2)  # 64*64*64 or 64*48*64 (crop sky)
             upsample4 = self.up_sampled_block_4(upsample3)  # 32*128*128 or 32*74*128 (crop sky)
-
+            
+            ### for segmentation
             out_seg = self.last_bn(
                 self.last_conv_segmentation(upsample4)
             )  # nb_class_segmentation*128*128
+
+            ### For image reconstruction
+            # out_seg = nn.functional.relu(self.last_conv_segmentation(upsample4))
 
 
         # Classification branch, traffic_light (+ state), intersection or none
@@ -245,7 +248,10 @@ class Model_Segmentation_Traffic_Light_Supervised(nn.Module):
             dist_to_tl_output = self.fc2_distance_to_tl(traffic_light_state_net)
         
         if self.dist_car:
-            dist_to_frontcar = self.fc_dist_to_frontcar(classif_state_net)
+            # dist_to_frontcar = self.fc_dist_to_frontcar(classif_state_net)
+            dist_to_frontcar = self.fc1_dist_to_frontcar(classif_state_net)
+            dist_to_frontcar = nn.functional.relu(dist_to_frontcar)
+            dist_to_frontcar = self.fc2_dist_to_frontcar(dist_to_frontcar)
 
         if self.action:
             if self.use_sensor and self.use_aux:

train.py
@@ -17,18 +17,6 @@ from utils import skip_run, get_num_gpus
 # Initialize the config directory
 initialize(config_path="configs", job_name="vae")
 
-# with skip_run('skip', 'split_image_folder') as check, check():
-#     hparams = compose(config_name="config")
-#     hparams['camera'] = 'semantic'
-#     log = hparams['train_logs'][0]
-
-#     read_path = hparams['data_dir'] + 'raw' + '/' + log
-#     print(read_path)
-#     splitfolders.ratio(read_path,
-#                        output=hparams['data_dir'] + 'processed' + '/' + log,
-#                        seed=1337,
-#                        ratio=(.8, 0.1, 0.1),
-#                        shuffle=False)
 
 with skip_run('skip', 'behavior_cloning') as check, check():
     # Load the parameters
@@ -66,11 +54,11 @@ with skip_run('skip', 'behavior_cloning') as check, check():
                              callbacks=[checkpoint_callback])
         trainer.fit(model)
 
-with skip_run('skip', 'aux-adv') as check, check():
+with skip_run('run', 'aux-adv') as check, check():
     # Load the parameters
     hparams = compose(config_name="config", overrides=['model=imitation'])
-
-    ckpt_paths = ['logs/2021-12-12/imitation.ckpt', 'logs/2021-12-12/imitation-v1.ckpt']
+    ckpt_paths = ['logs/supervised_imager_withaux_factor12/imitation-epoch=21-val_loss=0.97-train_loss=0.66.ckpt']
+    
     for ckpt_path in ckpt_paths:
 
         # Random seed
@@ -81,27 +69,37 @@ with skip_run('skip', 'aux-adv') as check, check():
         checkpoint_callback = pl.callbacks.ModelCheckpoint(
             monitor='val_loss',
             dirpath=hparams.log_dir,
-            save_top_k=1,
-            filename='imitation',
+            # period=2,
+            save_top_k=-1,
+            filename='imitation-{epoch}-{val_loss:.2f}-{train_loss:.2f}',
             mode='min')
         logger = pl.loggers.TensorBoardLogger(hparams.log_dir,
                                               name='imitation')
 
         # All this magic number should match the one used when training supervised...
-        net = Model_Segmentation_Traffic_Light_Supervised(1, 1, 1024, 6, 4, True, pretrained=True)
+        net = Model_Segmentation_Traffic_Light_Supervised(hparams)
+        
+        # if want to load weights
         selected_subnets = ['fc_action']
         net.loadWeights(ckpt_path=ckpt_path, selected_subnet=selected_subnets, exclude_mode=True)
         net.freezeLayers(selected_subnet=selected_subnets, exclude_mode=True)
         
         # output = net(net.example_input_array)
         # print(output)  # verification
-        data_loader = imitation_dataset.sequential_train_val_test_iterator(hparams)
+        data_loader = imitation_dataset.sequential_train_val_test_iterator(hparams, modes=['train', 'val'])
 
         model = Imitation(hparams, net, data_loader)
         trainer = pl.Trainer(gpus=gpus,
                              max_epochs=hparams.NUM_EPOCHS,
                              logger=logger,
                              callbacks=[checkpoint_callback])
+        
+        ### Log the Loss before training
+        trainer.validate(model, dataloaders=[data_loader['val_dataloader']])
+        model.val_loss_in_valStep = False
+        trainer.validate(model, dataloaders=[data_loader['train_dataloader']])
+        model.val_loss_in_valStep = True
+        
         trainer.fit(model)
 
 
@@ -152,34 +150,23 @@ with skip_run('skip', 'test') as check, check():
     # Load the parameters
     hparams = compose(config_name="config", overrides=['model=imitation'])
 
-    ckpt_paths = ['logs/2021-12-12/imitation.ckpt']
+    # ckpt_paths = ['logs/all_aux_supervised/2021-12-12/imitation.ckpt']
+    ckpt_paths = ['logs/supervised_image_recons_only/imitation-epoch=7-val_loss=0.07-train_loss=0.06.ckpt']
     for ckpt_path in ckpt_paths:
         # Random seed
-        gpus = get_num_gpus()
         torch.manual_seed(hparams.pytorch_seed)
 
-        # Checkpoint
-        checkpoint_callback = pl.callbacks.ModelCheckpoint(
-            monitor='val_loss',
-            dirpath=hparams.log_dir,
-            save_top_k=1,
-            filename='imitation',
-            mode='min')
-        logger = pl.loggers.TensorBoardLogger(hparams.log_dir,
-                                              name='imitation')
-
         # Setup
-        # net = CNNAuxNet(hparams)
-        net = Model_Segmentation_Traffic_Light_Supervised(1, 1, 1024, 6, 4, True, pretrained=True)
+        net = Model_Segmentation_Traffic_Light_Supervised(hparams)
         # output = net(net.example_input_array)
         # print(output)  # verification
 
         data_loader = imitation_dataset.sequential_train_val_test_iterator(
-            hparams, modes=['val'])
-        
+            hparams, modes=[ 'train_trash'])
+
         model = Imitation(hparams, net, data_loader)
         ckpt = ckpt_path
         model = model.load_from_checkpoint(ckpt, hparams=hparams, net=net, data_loader=data_loader)
-
+        
         # model.calcAccuracy(dataset_type='val')
-        model.sampleOutput(dataset_type='val')
+        model.sampleOutput(dataset_type='train_trash')